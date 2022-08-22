d0b7465a06aa11b84d4719b04055c7367fea0ace
Acobeats <fgarcialollini@gmail.com>
2022-08-22 02:21:19 -0300

    Reorganizacion

---
+++ Client/src/components/Contact.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {Form} from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import '../App.css';
+import '../Styles/Contact.css';
+import '../Styles/Landing.css';
+
+
+function Contact(){
+let emailName
+let messageName
+
+    const handleSubmit = async e => {
+       
+        if ( !emailName  || !messageName ){
+            alert('Sos un puto Escribi algo'); 
+            console.log('adan putete')
+            return
+        }
+        e.preventDefault ();
+        console.log('aca apreto un putete')
+    }
+    return(
+        <div className="home-div">
+        <Form id="form-size" onSubmit={handleSubmit}>
+            <Form.Group  className="form-letter" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control className='form-letter' type="email" placeholder="name@example.com" name="emailName" />
+            </Form.Group>
+            <Form.Group className="form-letter" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={3} name="messageName"/>
+            </Form.Group>
+            
+            <div className="form-button">
+            <button  className='cssbuttons-io-button' type="submit">Send 
+                <div className="icon">
+                    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor">
+                        </path>
+                    </svg>
+                </div>  
+            </button>
+            </div>
+            
+        </Form>
+                <div>
+                    <Link to='/home'>Go Back</Link>
+                </div>
+        </div>
+
+
+
+    );
+}
+
+export default Contact;
